0e54f8a68872e8f126d3d8a0998ecbe7cb957344
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2023-08-05 04:11:51 +0000

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,77 @@
+// page
+function hideShow(){
+    const showMe = document.querySelector('#page');
+
+   if (showMe.style.display === "none"){
+    showMe.style.display = 'block'
+   }
+   else{
+    showMe.style.display = 'none';
+   }
+
+}
+
+// page1
+function hideShow1() {
+   const showMe1 = document.querySelector('#page1');
+
+   if (showMe1.style.display === 'none') {
+    showMe1.style.display = 'block';
+    loop:false;
+   }
+   else{
+    showMe1.style.display = 'none'
+   }
+
+}
+
+// page2
+function hideShow2() {
+    const showMe2 = document.querySelector('#page2');
+
+    if(showMe2.style.display === 'none') {
+        showMe2.style.display = 'block';
+
+    }
+    else{
+        showMe2.style.display = 'none';
+    }
+}
+
+// page3
+function hideShow3() {
+    const showMe3 = document.querySelector('#page3');
+
+    if( showMe3.style.display === 'none') {
+        showMe3.style.display = 'block';
+
+    }
+    else{
+        showMe3.style.display = 'none';
+    }
+}
+
+// page4
+function hideShow4() {
+    const showMe4 = document.querySelector('#page4');
+
+    if( showMe4.style.display === 'none') {
+        showMe4.style.display = 'block';
+
+    }
+    else{
+        showMe4.style.display = 'none';
+    }
+}
+// page5
+function hideShow5() {
+    const showMe5 = document.querySelector('#page5');
+
+    if( showMe5.style.display === 'none') {
+        showMe5.style.display = 'block';
+
+    }
+    else{
+        showMe5.style.display = 'none';
+    }
+}
